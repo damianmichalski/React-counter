8765410202ab2d8efdef1f809785e85e43a0fbd1
DamianM <damian.michalski@kramp.com>
2018-12-11 13:37:11 +0100

    React counter

--- script.js
+++ script.js
@@ -16,6 +16,44 @@ var Counter = React.createClass({
         });
     },
 
+    componentWillMount: function() {
+        console.log('Before render!');
+    },
+
+
+    render: function() {
+        return React.createElement('div', {}, 'Zacznij liczyć ',
+            React.createElement('div', {className: 'alert alert-success'}, 'Licznik ' + this.state.counter),
+            React.createElement('button', {className: 'btn btn-success', onClick: this.increment}, 'Dodaj'),
+            React.createElement('button', {className: 'btn btn-warning', onClick: this.decrement}, 'Odejmij')
+        )
+    },
+    componentDidMount: function() {
+        console.log('After render!');
+    },
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
+
+var CounterSecond = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 5
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
 
     render: function() {
         return React.createElement('div', {}, 'Zacznij liczyć ',
@@ -26,5 +64,5 @@ var Counter = React.createClass({
     }
 });
 
-var element = React.createElement(Counter);
-ReactDOM.render(element, document.getElementById('app'));
+var elementSecond = React.createElement(CounterSecond);
+ReactDOM.render(elementSecond, document.getElementById('app2'));
